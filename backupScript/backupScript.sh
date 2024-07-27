5196c936fe4b2ff3b25480f64ac55149a873d0fe
MrRinkukumar2004 <rinkukumarsaini9799118495@gmail.com>
2024-07-27 08:38:46 +0530

    refactor

--- backupScript/backupScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-SOURCE="/home/rinku/data1/Devops/Github"
-DESTINATION="/home/rinku/data2"
-DATE=$(date +%Y-%m-%d_%H_%M-%S)
-# Create  backup Dirctory and copy file
-mkdir -p $DESTINATION/$DATE
-cp -rv $SOURCE $DESTINATION/$DATE
-echo "$DATE Backup completed on"
